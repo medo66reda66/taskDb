e56ec04dac5398dc04725b803ee034a5d6ef6806
Mahmoud_Zahra <medo66reda6677@gmail.com>
2025-09-13 23:07:56 +0300

    Add files via upload

---
+++ Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDatabase.Models
+{
+    public class Store
+    {
+        public int StoreId { get; set; }
+        public string Name { get; set; }
+        public List<Sale> sales { get; set; }
+    }
+}
